34cdffa82298c2ddd67bc56228092d850226a344
a833-u <akansara833@gmail.com>
2024-09-10 21:54:12 +0530

    Installing Shadcn ui react-router etc.

---
+++ Level2/Task1/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="">
+      <Button variant="default">Login</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ Level2/Task1/vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url';
+import {path, dirname} from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },  
+  },
+})
